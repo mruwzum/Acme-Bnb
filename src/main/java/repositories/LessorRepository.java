c8b6d00faa25970e9071ef85023b37e7847585cc
David De la torre <daviddelatorre@MacBook-Air-de-David.local>
2017-02-21 12:38:18 +0100

    Añadido los servicios básicos de lessor.
    -Gestionar sus propiedades
    -Aceptar o denegar reservas
    -Ver el total de fianza que tiene que pagar

--- src/main/java/repositories/LessorRepository.java
+++ src/main/java/repositories/LessorRepository.java
@@ -10,9 +10,10 @@ import java.util.Collection;
 
 @Repository
 public interface LessorRepository extends JpaRepository<Lessor, Integer> {
+    @Query("select c from Lessor c where c.userAccount.id = ?1")
+    Lessor findByUserAccountId(int userAccountId);
 
-
-    @Query("select u.propertys from Lessor u")
-    Collection<Property> getAllProperties();
+    @Query("select u.propertys from Lessor u where u.id =?1")
+    Collection<Property> getAllProperties(int id);
 
 }
--- src/main/java/services/LessorService.java
+++ src/main/java/services/LessorService.java
@@ -1,14 +1,16 @@
 package services;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import javax.transaction.Transactional;
 
+import domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
-import domain.Lessor;
 import repositories.LessorRepository;
 import security.LoginService;
 import security.UserAccount;
@@ -21,12 +23,16 @@ public class LessorService {
 	@Autowired
 	private LessorRepository lessorRepository;
 
+
 	// Constructor -------------------------------
 	public LessorService() {
 		super();
 	}
 
 	// Supporting services -----------------------
+	@Autowired
+	private PropertyService propertyService;
+
 
 	// Simple CRUD methods -----------------------
 	
@@ -63,4 +69,109 @@ public class LessorService {
 
 	// Other business methods -----------------------
 
+	public Lessor findByPrincipal() {
+		Lessor result;
+		UserAccount userAccount;
+
+		userAccount = LoginService.getPrincipal();
+		Assert.notNull(userAccount);
+		result = findByUserAccount(userAccount);
+		Assert.notNull(result);
+
+		return result;
+	}
+
+	private Lessor findByUserAccount(UserAccount userAccount) {
+		Assert.notNull(userAccount);
+
+		Lessor result;
+
+		result = lessorRepository.findByUserAccountId(userAccount.getId());
+
+		return result;
+	}
+
+
+	public Collection<Property> getAllProperties() {
+
+		Collection<Property> res;
+		Lessor u = findByPrincipal();
+		Assert.notNull(u, "user not identified");
+		res = lessorRepository.getAllProperties(u.getId());
+		return res;
+
+	}
+
+	public void deleteProperty(Property p) {
+		Assert.notNull(p, "the property doesn't exist");
+		Lessor u = findByPrincipal();
+		Assert.notNull(u, "user not identified");
+		Assert.isTrue(u.getPropertys().contains(p), "this isn't a property of Lessor " + u);
+		propertyService.delete(p);
+	}
+
+	public void registerProperty(Property p) {
+		Assert.notNull(p, "the property doesn't exist");
+		Lessor u = findByPrincipal();
+		Assert.notNull(u, "user not identified");
+		Assert.isTrue(!u.getPropertys().contains(p), "this lessor already register this property");
+		propertyService.save(p);
+
+	}
+
+	public Property modifyProperty(Property p, String name, String description, String address, String province,
+								   String country, String state, int capacity, String city) {
+
+		Assert.notNull(p);
+		Lessor u = findByPrincipal();
+		Assert.notNull(u, "user not identified");
+		Assert.isTrue(u.getPropertys().contains(p), "this isn't a property of Lessor " + u);
+
+		p.setName(name);
+		p.setDescription(description);
+		p.setAddress(address);
+		p.setProvince(province);
+		p.setCountry(country);
+		p.setState(state);
+		p.setCapacity(capacity);
+		p.setCity(city);
+
+		return p;
+
+	}
+
+	public void approveRequest(BookRequest bookRequest) {
+
+		//TODO aquí habría que comprobar que la bookRequest pertenece a alguna propiedad del lessor, pero no se como hacerlo sin poner un for
+		bookRequest.setStatus(RequestStatus.ACEPTED);
+
+	}
+
+	public void denyRequest(BookRequest bookRequest) {
+		//TODO aquí habría que comprobar que la bookRequest pertenece a alguna propiedad del lessor, pero no se como hacerlo sin poner un for
+		bookRequest.setStatus(RequestStatus.DENIED);
+	}
+
+	public Collection<BookRequest> getAllRequest() {
+		List<BookRequest> requests = new ArrayList<>();
+		Lessor u = findByPrincipal();
+		Assert.notNull(u, "user not identified");
+		for (Property p : u.getPropertys()) {
+			requests.addAll(p.getBookRequests());
+		}
+		Assert.notEmpty(requests, "No book request found");
+		return requests;
+	}
+
+	//TODO Una vez se establezca una propieda que sea fee y que solo pueda ser gestionada por el admin, se sustituye aquí
+	public Double totalFee() {
+		Double res = 0.0;
+		for (BookRequest b : getAllRequest()) {
+			if (b.getStatus().equals(RequestStatus.ACEPTED)) {
+				res = res + 1.0;
+			}
+		}
+		return res;
+
+	}
 }
