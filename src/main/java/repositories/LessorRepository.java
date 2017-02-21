6fa7641fe2df53db6e9ac217c813be1ee9c2a6a9
David De la torre <daviddelatorre@MacBook-Air-de-David.local>
2017-02-21 10:46:45 +0100

    Probando rama

--- src/main/java/repositories/LessorRepository.java
+++ src/main/java/repositories/LessorRepository.java
@@ -1,11 +1,18 @@
 package repositories;
 
+import domain.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import domain.Lessor;
 
+import java.util.Collection;
+
 @Repository
 public interface LessorRepository extends JpaRepository<Lessor, Integer> {
+
+
+    @Query("select u.propertys from Lessor u")
+    Collection<Property> getAllProperties();
 
 }
