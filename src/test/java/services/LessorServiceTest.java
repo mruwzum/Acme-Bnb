814fbba5cf559a2eece129ce41b6f2376914e37d
David De la torre <daviddelatorre@MacBook-Air-de-David.local>
2017-02-28 12:26:38 +0100

    Añadidos tests

---
+++ src/test/java/services/LessorServiceTest.java
@@ -0,0 +1,81 @@
+package services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by daviddelatorre on 28/2/17.
+ */
+public class LessorServiceTest extends AbstractTest {
+    @Test
+    public void create() throws Exception {
+
+    }
+
+    @Test
+    public void findOne() throws Exception {
+
+    }
+
+    @Test
+    public void findAll() throws Exception {
+
+    }
+
+    @Test
+    public void save() throws Exception {
+
+    }
+
+    @Test
+    public void delete() throws Exception {
+
+    }
+
+    @Test
+    public void findByPrincipal() throws Exception {
+
+    }
+
+    @Test
+    public void getAllProperties() throws Exception {
+
+    }
+
+    @Test
+    public void deleteProperty() throws Exception {
+
+    }
+
+    @Test
+    public void registerProperty() throws Exception {
+
+    }
+
+    @Test
+    public void modifyProperty() throws Exception {
+
+    }
+
+    @Test
+    public void approveRequest() throws Exception {
+
+    }
+
+    @Test
+    public void denyRequest() throws Exception {
+
+    }
+
+    @Test
+    public void getAllRequest() throws Exception {
+
+    }
+
+    @Test
+    public void totalFee() throws Exception {
+
+    }
+
+}
