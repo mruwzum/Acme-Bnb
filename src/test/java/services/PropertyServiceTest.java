cdfe94ddea6ccf3deb8c3d601efe902f35543433
mruwzum <daviddelatorre00@gmail.com>
2017-02-28 12:45:39 +0100

    fg

---
+++ src/test/java/services/PropertyServiceTest.java
@@ -0,0 +1,44 @@
+package services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mruwzum on 28/2/17.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/datasource.xml",
+        "classpath:spring/config/packages.xml"})
+@Transactional
+public class PropertyServiceTest extends AbstractTest {
+    @Test
+    public void create() throws Exception {
+
+    }
+
+    @Test
+    public void findOne() throws Exception {
+
+    }
+
+    @Test
+    public void findAll() throws Exception {
+
+    }
+
+    @Test
+    public void save() throws Exception {
+
+    }
+
+    @Test
+    public void delete() throws Exception {
+
+    }
+
+}
